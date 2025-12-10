cd27100cc82eacd9050e333c9887b30a82f7cdc8
tristan-the-G <gtristan884@gmail.com>
2025-12-10 15:07:08 +0100

    Updated HeroSection.tsx

--- components/sections/HeroSection.tsx
+++ components/sections/HeroSection.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { useLanguage } from '@/contexts/LanguageContext';
+import { getTexts } from '@/lib/translations';
 import { useState } from 'react';
 import { Button } from '@/components/ui/button';
 import { Badge } from '@/components/ui/badge';
